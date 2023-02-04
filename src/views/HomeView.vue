986497dcd953cd3688faa144d23c7cea938cc1bb
kokihata <hatakekoki@gmail.com>
2023-02-04 21:28:18 +0900

    add navbar

---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app id="home">
+    <NavBar />
+    <v-container fluid> </v-container>
+  </v-app>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import NavBar from "../components/NavBar.vue";
+// Components
+
+export default defineComponent({
+  name: "HomeView",
+
+  components: {
+    NavBar,
+  },
+});
+</script>
